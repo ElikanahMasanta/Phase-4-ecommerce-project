d89d866ca792e6dfd620e662473cf414fdfcb238
ElikanahMasanta <emasanta8@gmail.com>
2022-12-07 22:41:22 +0300

    sessions and users controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   
+  namespace :store do
+    
+    post "/signup", to: "users#create"
+    get "/me", to: "user#show"
+
+  end
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
